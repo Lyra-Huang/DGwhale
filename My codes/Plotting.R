aa5d6e57b61c715a2e2cd2d08553381874210e87
Lyra-Huang <66240005+Lyra-Huang@users.noreply.github.com>
2021-03-16 14:49:52 +1100

    update

--- My codes/Plotting.R
+++ My codes/Plotting.R
@@ -8,5 +8,20 @@ ggplot(mean_chl, aes(x=time, y=mean, fill=location)) +
 
 
 ##scattered plot
-ggplot(data=nw, aes(x=Sepal.Length, Petal.Length)) +
-  geom_point()
+ggplot(data=se, aes(x= mean_sst,whale_presence)) +
+   geom_point(aes(colour = month)) #+
+   # geom_smooth(method=lm , color="red", se=FALSE)
+
+ggplot(data=nw, aes(x= chl_small,whale_presence)) +
+  geom_point(aes(colour = month)) +
+  geom_smooth(method=lm , color="red", se=FALSE)
+
+
+ggplot(data=se, aes(x=whale_presence, chl_small)) +
+  geom_point()+
+  geom_smooth(method=lm , color="red", se=FALSE)
+
+a$month <- as.character(a$month)
+ggplot(a, aes(x=sst, y=nb_calls)) +
+  geom_point(aes(colour= month)) +
+  theme_classic()
